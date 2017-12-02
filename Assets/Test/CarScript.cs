302edabb35f1d80330fc6fa287904d48978d184e
Jose Moreno <josem1988@gmail.com>
2017-12-02 20:07:35 +0100

    +Test scene

--- Assets/Test/CarScript.cs
+++ Assets/Test/CarScript.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarScript : MonoBehaviour {
+public class CarScript : MonoBehaviour
+{
+
+	public float vel;
+	public float rotvel;
+	public Transform camtransform;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position -= vel * transform.right * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (new Vector3 (0f, 0f, rotvel * Time.deltaTime));
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (new Vector3 (0f, 0f, -rotvel * Time.deltaTime));
+		}
+
+
+		camtransform.position = transform.position - new Vector3 (0f, 0f, 10f);
 	}
 }
