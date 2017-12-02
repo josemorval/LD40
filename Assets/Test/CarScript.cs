d4de4e8acd32ed202a61601c0b4bd492cf27094d
Jose Moreno <josem1988@gmail.com>
2017-12-03 00:02:41 +0100

    +Primera iteración controlado coche

--- Assets/Test/CarScript.cs
+++ Assets/Test/CarScript.cs
@@ -7,28 +7,45 @@ public class CarScript : MonoBehaviour
 
 	public float vel;
 	public float rotvel;
+	public float otherrotvel;
 	public Transform camtransform;
+	public Rigidbody2D rigidbody;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		rigidbody = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			transform.position -= vel * transform.right * Time.deltaTime;
+			rigidbody.AddForce (-transform.right * vel * Time.deltaTime);
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			rigidbody.AddForce (transform.right * vel * Time.deltaTime);
 		}
 
+		Vector2 v = new Vector2 (transform.up.x, transform.up.y);
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.Rotate (new Vector3 (0f, 0f, rotvel * Time.deltaTime));
+			rigidbody.velocity = rigidbody.velocity.magnitude * RotateVector (rigidbody.velocity.normalized, -rotvel * Time.deltaTime);
+			transform.Rotate (new Vector3 (0f, 0f, otherrotvel * Time.deltaTime));
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.Rotate (new Vector3 (0f, 0f, -rotvel * Time.deltaTime));
+			rigidbody.velocity = rigidbody.velocity.magnitude * RotateVector (rigidbody.velocity.normalized, rotvel * Time.deltaTime);
+			transform.Rotate (new Vector3 (0f, 0f, -otherrotvel * Time.deltaTime));
+
+
 		}
-
-
+			
 		camtransform.position = transform.position - new Vector3 (0f, 0f, 10f);
+	}
+
+	public Vector2 RotateVector (Vector2 v, float angle)
+	{
+		Vector2 vv = Vector2.zero;
+		vv.x = Mathf.Cos (angle) * v.x + Mathf.Sin (angle) * v.y;
+		vv.y = -Mathf.Sin (angle) * v.x + Mathf.Cos (angle) * v.y;
+
+		return vv;
 	}
 }
