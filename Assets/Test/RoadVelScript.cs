a9ac01199d13afde882b3ff264070371da35595c
Jose Moreno <josem1988@gmail.com>
2017-12-02 19:07:59 +0100

    +Cosillas de test

---
+++ Assets/Test/RoadVelScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadVelScript : MonoBehaviour
+{
+
+	public Material mat;
+
+	void Start ()
+	{
+		mat = GetComponent<MeshRenderer> ().sharedMaterial;
+	}
+
+	void Update ()
+	{
+		mat.mainTextureOffset += new Vector2 (0f, Time.deltaTime);
+	}
+}
