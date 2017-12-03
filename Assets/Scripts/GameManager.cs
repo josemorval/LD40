81c705609c98ea33554707c713a526d75210b2c4
Jose Moreno <josem1988@gmail.com>
2017-12-03 17:26:52 +0100

    +Estructura basica

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	[SerializeField]
+	private Transform _cameraTransform;
+
+	[SerializeField]
+	private Transform _introContentTransform;
+	[SerializeField]
+	private Transform _gameplayContentTransform;
+	[SerializeField]
+	private Transform _gameoverContentTransform;
+
+}
