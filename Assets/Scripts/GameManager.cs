212e09e48a322e6bf62c632f904d6f69569f71c2
Jose Moreno <josem1988@gmail.com>
2017-12-03 17:47:43 +0100

    +Update structure

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,72 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-
 	void Start ()
 	{
-		
+		SetupIntroLevel ();
 	}
 
 	void Update ()
 	{
+		if (_gameState == STATE_INTRO) {
+			UpdateIntroLevel ();
+		} else if (_gameState == STATE_GAMEPLAY) {
+			UpdateGameplayLevel ();
+		} else if (_gameState == STATE_GAMEOVER) {
+			UpdateGameoverLevel ();
+		}
+
+		//Do nothing
+	}
+
+
+	public void SetupIntroLevel ()
+	{
+		ActivateLevels (false);
+		_gameState = STATE_INTRO;
+	}
+
+	public void SetupGameplayLevel ()
+	{
+		ActivateLevels (false);
+		_gameState = STATE_GAMEPLAY;
+		_globalTime = 0f;
+
+	}
+
+	public void SetupGameOverLevel ()
+	{
+		ActivateLevels (false);
+		_gameState = STATE_GAMEOVER;
+	}
+
+	public void UpdateIntroLevel ()
+	{
+
+	}
+
+	public void UpdateGameplayLevel ()
+	{
 		
 	}
+
+	public void UpdateGameoverLevel ()
+	{
+		
+	}
+
+	public void ActivateLevels (bool activate)
+	{
+		ActivateLevels (activate, activate, activate);
+	}
+
+	public void ActivateLevels (bool a, bool b, bool c)
+	{
+		_introContentTransform.gameObject.SetActive (a);
+		_gameplayContentTransform.gameObject.SetActive (b);
+		_gameoverContentTransform.gameObject.SetActive (c);
+	}
+
 
 	[SerializeField]
 	private Transform _cameraTransform;
@@ -25,5 +81,19 @@ public class GameManager : MonoBehaviour
 	private Transform _gameplayContentTransform;
 	[SerializeField]
 	private Transform _gameoverContentTransform;
+
+	[SerializeField]
+	private float _dreamLevel;
+	[SerializeField]
+	private float _timeCheckpoint;
+	[SerializeField]
+	private float _globalTime;
+
+	[SerializeField]
+	private int _gameState;
+
+	private int STATE_INTRO = 0;
+	private int STATE_GAMEPLAY = 10;
+	private int STATE_GAMEOVER = 100;
 
 }
