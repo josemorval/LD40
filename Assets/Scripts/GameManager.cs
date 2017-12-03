fe11bedff92b53d2a57411104eaf81720e6e5bf7
Jose Moreno <josem1988@gmail.com>
2017-12-03 20:05:03 +0100

    +Update flujo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 
 	void Start ()
 	{
+		ActivateLevels (false, false, false, false);
 		SetupIntroLevel ();
 	}
 
@@ -26,49 +27,72 @@ public class GameManager : MonoBehaviour
 
 	public void SetupIntroLevel ()
 	{
-		ActivateLevels (false);
+		ActivateLevels (true, false, false, false);
 		_gameState = STATE_INTRO;
+		_cameraTransform.position = _introContentTransform.position + new Vector3 (0f, 0f, -10f);
 	}
 
 	public void SetupGameplayLevel ()
 	{
-		ActivateLevels (false);
+		ActivateLevels (false, true, false, true);
 		_gameState = STATE_GAMEPLAY;
 		_globalTime = 0f;
+		_dreamLevel = 1f;
+		_timeCheckpoint = _initTimeCheckPoint;
+		_cameraTransform.position = _gameplayContentTransform.position + new Vector3 (0f, 0f, -10f);
 
 	}
 
 	public void SetupGameOverLevel ()
 	{
-		ActivateLevels (false);
+		ActivateLevels (true, false, true, false);
 		_gameState = STATE_GAMEOVER;
+		_cameraTransform.position = _gameoverContentTransform.position + new Vector3 (0f, 0f, -10f);
+
 	}
 
 	public void UpdateIntroLevel ()
 	{
-
+		if (Input.GetKeyDown (KeyCode.A)) {
+			SetupGameplayLevel ();
+		}
 	}
 
 	public void UpdateGameplayLevel ()
 	{
-		
+		_dreamLevel -= _currentRatioDreamLevel * Time.deltaTime;
+		_globalTime += Time.deltaTime;
+		_timeCheckpoint -= Time.deltaTime;
+
+		if (_dreamLevel < 0f || _timeCheckpoint < 0f) {
+			SetupGameOverLevel ();
+		} else {
+			_dreamLevelTransform.localScale = new Vector3 (_dreamLevel, 1f, 1f);
+			_timeCheckPointText.text = _timeCheckpoint.ToString ("#0") + "s";
+			_globalTimeText.text = _globalTime.ToString ("#0") + "s";
+		}
+
+	
 	}
 
 	public void UpdateGameoverLevel ()
 	{
-		
+		if (Input.GetKeyDown (KeyCode.A)) {
+			SetupIntroLevel ();
+		}	
 	}
 
 	public void ActivateLevels (bool activate)
 	{
-		ActivateLevels (activate, activate, activate);
+		ActivateLevels (activate, activate, activate, activate);
 	}
 
-	public void ActivateLevels (bool a, bool b, bool c)
+	public void ActivateLevels (bool a, bool b, bool c, bool d)
 	{
 		_introContentTransform.gameObject.SetActive (a);
 		_gameplayContentTransform.gameObject.SetActive (b);
 		_gameoverContentTransform.gameObject.SetActive (c);
+		_uiTransform.gameObject.SetActive (d);
 	}
 
 
@@ -83,14 +107,30 @@ public class GameManager : MonoBehaviour
 	private Transform _gameoverContentTransform;
 
 	[SerializeField]
-	private float _dreamLevel;
-	[SerializeField]
+	private Transform _uiTransform;
+
 	private float _timeCheckpoint;
 	[SerializeField]
+	private TextMesh _timeCheckPointText;
+
 	private float _globalTime;
+	[SerializeField]
+	private TextMesh _globalTimeText;
+
+	private float _dreamLevel;
+	[SerializeField]
+	private Transform _dreamLevelTransform;
+	[SerializeField]
+	private float _currentRatioDreamLevel;
+
+	[SerializeField]
+	private float _numberLaps;
 
 	[SerializeField]
 	private int _gameState;
+
+	[SerializeField]
+	private float _initTimeCheckPoint;
 
 	private int STATE_INTRO = 0;
 	private int STATE_GAMEPLAY = 10;
